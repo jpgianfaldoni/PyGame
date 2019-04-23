d32ad0df72515007de1f49d0d1a5e4dac68d0a02
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2019-04-23 17:29:10 -0300

    add colisao e mais asteroides

--- assets/asteroids-base.py
+++ assets/asteroids-base.py
@@ -3,10 +3,12 @@
 # Importando as bibliotecas necessárias.
 import pygame
 from os import path
+import random
+import time
 
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'img')
-
+snd_dir = path.join(path.dirname(__file__), 'snd')
 # Dados gerais do jogo.
 WIDTH = 480 # Largura da tela
 HEIGHT = 600 # Altura da tela
@@ -37,6 +39,7 @@ class Player(pygame.sprite.Sprite):
         
         
         self.speedx = 0
+        self.radius = 25
     #Metodo que atualiza a posicao da navinha
     def update(self):
         self.rect.x += self.speedx
@@ -59,12 +62,14 @@ class Mob(pygame.sprite.Sprite):
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 250
+        self.rect.bottom = HEIGHT - 600
         
         VX = random.randrange(-3,3)   
         VY = random.randrange(2,9)
         self.speedx = VX
         self.speedy = VY
+        
+        self.radius = int(self.rect.width * .85 / 2)
         
         
     #Metodo que atualiza a posicao da navinha
@@ -84,8 +89,8 @@ class Mob(pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left  < 0:
             self.rect.left = 0
-
         
+    
 
 # Inicialização do Pygame.
 pygame.init()
@@ -106,16 +111,46 @@ clock = pygame.time.Clock()
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 
+# Carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, "tgfcoder-FrozenJam-SeamlessLoop.ogg"))
+pygame.mixer.music.set_volume(0.4)
+boom_sound = pygame.mixer.Sound(path.join(snd_dir, "expl3.wav"))
+
+
 player = Player()
 mob = Mob()
+mob1 = Mob()
+mob2 = Mob()
+mob3 = Mob()
+mob4 = Mob()
+mob5 = Mob()
+mob6 = Mob()
+mob7 = Mob()
+mobs = pygame.sprite.Group()
+mobs.add(mob)
+mobs.add(mob1)
+mobs.add(mob2)
+mobs.add(mob3)
+mobs.add(mob4)
+mobs.add(mob5)
+mobs.add(mob6)
+mobs.add(mob7)
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(mob)
+all_sprites.add(mob1)
+all_sprites.add(mob2)
+all_sprites.add(mob3)
+all_sprites.add(mob4)
+all_sprites.add(mob5)
+all_sprites.add(mob6)
+all_sprites.add(mob7)
 
 # Comando para evitar travamentos.
 try:
     
     # Loop principal.
+    pygame.mixer.music.play(loops=-1)
     running = True
     while running:
         
@@ -151,7 +186,14 @@ try:
 
 
                     
-        all_sprites.update()                    
+        all_sprites.update()                  
+        
+        #Verifica colisao
+        hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+        if hits:
+            boom_sound.play()
+            time.sleep(1)
+            running = False
                     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
